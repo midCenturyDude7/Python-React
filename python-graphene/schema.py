0b1af40ad8ca714567740ac91d5a38bc47bea7f4
midCenturyDude7 <mgriffes@gmail.com>
2021-12-27 21:23:35 -0800

    latest updates

---
+++ python-graphene/schema.py
@@ -0,0 +1,26 @@
+import graphene
+import json
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+    is_admin = graphene.Boolean()
+
+    def resolve_hello(self, info):
+        return "world"
+
+    def resolve_is_admin(self, info):
+        return True
+
+
+schema = graphene.Schema(query=Query, auto_camelcase=False)
+
+result = schema.execute( 
+    '''
+    {
+        is_admin
+    }
+    '''
+)
+
+dictResult = dict(result.data.items())
+print(json.dumps(dictResult, indent=2))
