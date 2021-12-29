935ef28a4ef6c099e14ee9942f56387ce878a1c2
midCenturyDude7 <mgriffes@gmail.com>
2021-12-28 17:34:29 -0800

    Adding dynamic values

--- python-graphene/schema.py
+++ python-graphene/schema.py
@@ -1,11 +1,12 @@
 import graphene
 import json
-from datetime import date, datetime
+import uuid
+from datetime import datetime
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 
 class Query(graphene.ObjectType):
@@ -17,30 +18,41 @@ class Query(graphene.ObjectType):
     def resolve_hello(self, info):
         return "world"
 
-
     def resolve_is_admin(self, info):
         return True
-
 
     def resolve_users(self, info, limit=None):
         return [
             User(id="1", username="Fred", created_at=datetime.now()),
-            User(id="2", username="Doug", created_at=datetime.now()),
+            User(id="2", username="Doug", created_at=datetime.now())
         ][:limit]
 
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
 
-schema = graphene.Schema(query=Query, auto_camelcase=False)
+    class Arguments:
+        username = graphene.String()
+    
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 result = schema.execute( 
     '''
-    {
-        users(limit: 1) {
+    query getUsersQuery ($limit: Int) {
+        users(limit: $limit) {
             id
             username
-            created_at
+            createdAt
         }
     }
-    '''
+    ''',
+    variable_values={'limit': 1}
 )
 
 dictResult = dict(result.data.items())
